877850fbe4d9515717d4d0513328ea745df89c23
Chuanxiu Xiong <chuanxix@usc.edu>
2017-11-12 23:40:27 -0800

    commit - ethan

--- FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/AddToiletActivity.java
+++ FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/AddToiletActivity.java
@@ -1,18 +1,18 @@
-package itp341.pai.sonali.finalprojectfrontend;
-
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Window;
-
-public class AddToiletActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addtoilet);
+//package itp341.pai.sonali.finalprojectfrontend;
+//
+//import android.graphics.Color;
+//import android.graphics.drawable.ColorDrawable;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.Window;
+//
+//public class AddToiletActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_addtoilet);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 //
@@ -23,6 +23,6 @@ public class AddToiletActivity extends AppCompatActivity {
 //
 //// finally change the color
 //        window.setStatusBarColor(getResources().getColor(android.R.color.black));
-    }
-
-}
+//    }
+//
+//}
