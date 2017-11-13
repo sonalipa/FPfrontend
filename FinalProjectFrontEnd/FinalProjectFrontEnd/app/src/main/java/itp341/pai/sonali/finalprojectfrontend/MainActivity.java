19b9958bbc287556f4071600dab7802dc026ca97
Chuanxiu Xiong (Ethan) <chuanxix@usc.edu>
2017-11-12 20:43:22 -0800

    Add files via upload

--- FinalProjectFrontEnd/FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/MainActivity.java
+++ FinalProjectFrontEnd/FinalProjectFrontEnd/app/src/main/java/itp341/pai/sonali/finalprojectfrontend/MainActivity.java
@@ -1,17 +1,22 @@
 package itp341.pai.sonali.finalprojectfrontend;
 
-import android.app.ActionBar;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
+
+import java.net.URL;
+
+import itp341.pai.sonali.finalprojectfrontend.model.GET_HTTP;
+import itp341.pai.sonali.finalprojectfrontend.model.POST_HTTP;
 import itp341.pai.sonali.finalprojectfrontend.model.User;
 
 public class MainActivity extends AppCompatActivity {
@@ -52,6 +57,50 @@ public class MainActivity extends AppCompatActivity {
                 username = usernameInput.getText().toString();
                 password = passwordInput.getText().toString();
                 //TO DO: send these to data base to verify if user exists
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            // get connection to the "SignIn" Servlet
+                            URL url = new URL("http://localhost:8080/FinalProject/SignIn");
+                            POST_HTTP post_http = new POST_HTTP(url);
+                            post_http.addParameters("username", username);
+                            post_http.addParameters("password", password);
+                            post_http.connect();
+
+                            // return user JSON as the response Text. If the user does not exist, or
+                            //if the user's credentials are not correct - return an "ERROR" string
+
+
+                            String userJson = post_http.getResponse();
+
+                            if (userJson.equals("ERROR"))
+                            {
+                                //To-Do: show error messages
+
+
+
+                            }
+                            else
+                            {
+                                SharedPreferences.edit();
+
+
+                            }
+
+                            String storedHash = null;
+                        }catch (Exception e)
+                        {
+                            Log.d("Exception", e.toString());
+                    }
+                    }
+                });
+
+
+
+
+
+
 
                 //if exists, launch an intent to the listview page of all toilets
 
